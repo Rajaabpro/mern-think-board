56c6a95b08a2d1ba55c1d4229c565ac9efafb834
Rajaabpro <rajaab.pro@gmail.com>
2026-01-24 11:22:05 +0500

    Add PUT and DELETE endpoints for notes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,21 @@ const app = express();
 
 
 app.get('/api/notes', (req, res) => {
-    res.send('Notes') ;
+    res.json({ notes: [] });
 });
 
 app.post('/api/notes', (req, res) => {
     res.sendStatus(201).json({ message: 'Note created' });
 });
   
+app.put('/api/notes/:id', (req, res) => {
+    res.sendStatus(200).json({ message: 'Note updated' });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    res.sendStatus(200).json({ message: 'Note deleted' });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
