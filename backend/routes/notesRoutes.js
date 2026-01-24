b6f3af5f6ee68055038e714715ec28346e2c9758
Rajaabpro <rajaab.pro@gmail.com>
2026-01-24 12:26:53 +0500

    Add CRUD operations for notes in notesRoutes: implement GET, POST, PUT, and DELETE endpoints

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,6 +1,19 @@
 import express from 'express';
 
-const router = express.Router();
+ router = express.Router();
+router.get('/', (req, res) => {
+    res.sendStatus(200).send('Notes');
+});
 
+router.post('/', (req, res) => {
+    res.sendStatus(201).send('Note created');
+});
 
+router.put('/:id', (req, res) => {
+    res.sendStatus(200).send('Note updated');
+});
+
+router.delete('/:id', (req, res) => {
+    res.sendStatus(200).send('Note deleted');
+});
 export default router;
