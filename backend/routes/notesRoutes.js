85955d77c28834dda27811cec41a8aad8a23331b
Rajaabpro <rajaab.pro@gmail.com>
2026-01-24 13:08:49 +0500

    Refactor notesRoutes to utilize controller functions for CRUD operations

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,19 +1,8 @@
 import express from 'express';
-
- router = express.Router();
-router.get('/', (req, res) => {
-    res.sendStatus(200).send('Notes');
-});
-
-router.post('/', (req, res) => {
-    res.sendStatus(201).send('Note created');
-});
-
-router.put('/:id', (req, res) => {
-    res.sendStatus(200).send('Note updated');
-});
-
-router.delete('/:id', (req, res) => {
-    res.sendStatus(200).send('Note deleted');
-});
+import { getNotes, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
+router = express.Router();
+router.get('/', getNotes);
+router.post('/', createNote);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
 export default router;
