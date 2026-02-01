c153492d7c942281200ef2da0a98fb61a11f66db
Rajaabpro <rajaab.pro@gmail.com>
2026-02-01 08:03:06 +0500

    Refactor updateNote function in notesController: add error handling for non-existent notes and return the updated note in the response.

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -14,7 +14,7 @@ export async function getNotes(req, res) {
 export async function createNote(req, res) {
   try {
     const { title, content } = req.body;
-    const newNote = new Note({ title, content });x
+    const newNote = new Note({ title, content });
     await newNote.save();
     res.status(201).json(newNote);
   } catch (error) {
@@ -25,8 +25,11 @@ export async function createNote(req, res) {
 export async function updateNote(req, res) {
   try {
     const {title, content} = req.body;
-    await Note.findByIdAndUpdate(req.params.id, {title, content});
-    res.status(200).json({message: "Note updated"});
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content});
+    if (!updatedNote) {
+      return res.status(404).json({message: "Note not found"});
+    }
+    res.status(200).json(updatedNote);
   }
   catch (error) {
     console.error("Error updating note", error);
