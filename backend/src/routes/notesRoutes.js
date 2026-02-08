f7b498c8a87c2474f3a43ee1cffc6749af46581b
Rajaabpro <rajaab.pro@gmail.com>
2026-02-08 07:21:56 +0500

    Add getNoteById endpoint to notesRoutes: enable retrieval of a single note by its ID for improved note management.

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getNotes, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
+import { getNotes, getNoteById, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
 
 const router = express.Router();
 
 router.get('/getNotes', getNotes);
+router.get('/getNoteById/:id', getNoteById);
 router.post('/createNote', createNote);
 router.put('/updateNote/:id', updateNote);
 router.delete('/deleteNote/:id', deleteNote);
